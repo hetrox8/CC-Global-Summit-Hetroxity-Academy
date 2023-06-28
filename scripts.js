688aead3485c3764d555cbf506b8e464c7b17449
suleiman gacheru <dretrevor8@gmail.com>
2023-06-28 11:58:53 +0300

    make changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
 
+const hamburgerBtn = document.querySelector('.logo');
+const menu = document.querySelector('.mobile-nav');
+const closeMenu = document.querySelector('.x-icon');
+const menuOptions = menu.getElementsByTagName('li');
+hamburgerBtn.addEventListener('click', () => {
+  menu.classList.remove('hidden');
+  menu.classList.toggle('visible');
+});
+
+closeMenu.addEventListener('click', () => {
+  menu.classList.remove('visible');
+  menu.classList.toggle('hidden');
+});
+
+for (let i = 0; i < menuOptions.length; i += 1) {
+  menuOptions[i].addEventListener('click', () => {
+    menu.classList.remove('visible');
+    menu.classList.toggle('hidden');
+  });
+}
