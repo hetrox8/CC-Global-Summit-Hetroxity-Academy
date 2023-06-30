eda1058301dda35f6c174bf7855751e42bb3f704
suleiman gacheru <dretrevor8@gmail.com>
2023-06-30 13:47:46 +0300

    fix slinters

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ hamburgerBtn.addEventListener('click', () => {
   list.style.display = 'flex';
   hamburgerBtn.style.display = 'none';
   document.getElementById('x-icon').style.display = 'block';
-  for (let i = 0; i < hideElement.length; i++) {
+  for (let i = 0; i < hideElement.length; i += 1) {
     hideElement[i].style.display = 'none';
   }
 });
@@ -14,7 +14,7 @@ document.getElementById('x-icon').addEventListener('click', () => {
   list.style.display = '';
   hamburgerBtn.style.display = '';
   document.getElementById('x-icon').style.display = '';
-  for (let i = 0; i < hideElement.length; i++) {
+  for (let i = 0; i < hideElement.length; i += 1) {
     hideElement[i].style.display = '';
   }
 });
